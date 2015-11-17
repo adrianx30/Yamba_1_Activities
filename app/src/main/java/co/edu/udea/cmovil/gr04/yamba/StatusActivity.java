8f0d66991d351e96f7003069f36195a40b2e86ce
adrianx30 <adrianx30@gmail.com>
2015-11-16 22:01:02 -0500

    Laboratorio Yamba 2 (Fragments)

--- app/src/main/java/co/edu/udea/cmovil/gr04/yamba/StatusActivity.java
+++ app/src/main/java/co/edu/udea/cmovil/gr04/yamba/StatusActivity.java
@@ -2,6 +2,8 @@ package co.edu.udea.cmovil.gr04.yamba;
 
 import com.thenewcircle.yamba.client.YambaClient;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -16,9 +18,12 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,15 +33,45 @@ import android.widget.Toast;
 public class StatusActivity extends AppCompatActivity {
 
     private static final String TAG = StatusActivity.class.getSimpleName();
-    private Button mButtonTweet;
+    /*private Button mButtonTweet;
     private EditText mTextStatus;
     private TextView mTextCount;
-    private int mDefaultColor;
+    private int mDefaultColor;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+        //WindowManager wm = getWindowManager();
+        //Display d = wm.getDefaultDisplay();
+        //FragmentManager fragmentManager = getFragmentManager();
+        //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        /*if (d.getRotation()== Surface.ROTATION_90 )
+        {
+            //---landscape mode---
+            StatusFragment fragment1 = new StatusFragment();
+            // android.R.id.content refers to the content
+            // view of the activity
+            fragmentTransaction.replace(android.R.id.content, fragment1);
+        }
+        else{
+            //---portrait mode---
+            StatusFragment fragment2 = new StatusFragment();
+            fragmentTransaction.replace(android.R.id.content, fragment2);
+        }
+        fragmentTransaction.commit();//se llama el metodo comit para efectuar//los cambios */
+
+        if (savedInstanceState == null) {
+            // Create a fragment
+            StatusFragment fragment = new StatusFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(android.R.id.content, fragment,  fragment.getClass().getSimpleName());
+            fragmentTransaction.commit();
+        }
+
 
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -48,7 +83,7 @@ public class StatusActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
         mButtonTweet = (Button) findViewById(R.id.status_button_tweet);
         mTextStatus = (EditText) findViewById(R.id.status_text);
         mTextCount = (TextView) findViewById(R.id.status_text_count);
@@ -61,8 +96,10 @@ public class StatusActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String status = mTextStatus.getText().toString();
                 PostTask postTask = new PostTask();
+                //Oculta el teclado luego de darle click al boton
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(mTextStatus.getWindowToken(), 0);
+
                 postTask.execute(status);
                 Log.d(TAG, "onClicked");
             }
@@ -75,7 +112,7 @@ public class StatusActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 int count = 140 - s.length();
                 mTextCount.setText(Integer.toString(count));
-
+                if (count != 140) mButtonTweet.setEnabled(true); else mButtonTweet.setEnabled(false);
                 if (count < 50) {
                     mTextCount.setTextColor(Color.RED);
                 } else {
@@ -95,7 +132,7 @@ public class StatusActivity extends AppCompatActivity {
 
         });
 
-        Log.d(TAG, "onCreated");
+        Log.d(TAG, "onCreated"); */
     }
 
 
@@ -122,7 +159,7 @@ public class StatusActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    class PostTask extends AsyncTask<String, Void, String> {
+    /*class PostTask extends AsyncTask<String, Void, String> {
         private ProgressDialog progress;
 
         @Override
@@ -145,6 +182,8 @@ public class StatusActivity extends AppCompatActivity {
                 cloud.postStatus(params[0]);
 
                 Log.d(TAG, "Successfully posted to the cloud: " + params[0]);
+
+
                 return "Successfully posted";
             } catch (Exception e) {
                 Log.e(TAG, "Failed to post to the cloud", e);
@@ -158,9 +197,9 @@ public class StatusActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             progress.dismiss();
             if (this != null && result != null)
-                Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
-                        .show();
+                Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+            mTextStatus.setText("");
         }
 
-    }
+    }*/
 }
